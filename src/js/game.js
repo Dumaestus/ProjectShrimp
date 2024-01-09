258b2ea8ea6c78cfd27b076c2ed661739e715bbe
Demeas <dumaestus@gmail.com>
2024-01-09 14:16:39 -0500

    Renamed index.js to game.js
    For ease of use

--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,4 @@
 const CANVAS = document.querySelector('canvas'); // Returns the first Element that matches the specific selector, in this case our canvas element in index.html
 const CONTEXT = CANVAS.getContext('2d'); //Returns a drawing context on the canvas. We're basically using 2d methods to draw
 
-CANVAS.width = 1024; // 16:9 ratio to fit most screen sizes
-CANVAS.height = 576;
-
 CANVAS.fillStyle = 'white'
