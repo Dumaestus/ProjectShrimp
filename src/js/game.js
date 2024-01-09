2d44623932c7465a867cc35b6f2f35b9fe50c5c6
Demeas <dumaestus@gmail.com>
2024-01-09 14:27:47 -0500

    Moved game logic from index.html into game.js

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,30 @@
-const CANVAS = document.querySelector('canvas'); // Returns the first Element that matches the specific selector, in this case our canvas element in index.html
-const CONTEXT = CANVAS.getContext('2d'); //Returns a drawing context on the canvas. We're basically using 2d methods to draw
+// Create PIXI Application
+const app = new PIXI.Application({
+    width: 640,
+    height: 360
+});
 
-CANVAS.fillStyle = 'white'
+// Sets scale mode to nearest
+PIXI.BaseTexture.defaultOptions.scaleMode = 0;
+
+// Add the PIXI canvas to the HTML body
+document.body.appendChild(app.view);
+
+const background = PIXI.Sprite.from('images/game_background.png');
+background.scale.set(2,2);
+app.stage.addChild(background);
+
+// Write player to screen
+// const cat = PIXI.Sprite.from('images/player/')
+
+// Load a sprite image by assigning 'PLAYER' to its resource (png)
+/*
+app.loader.add('PLAYER', 'images/testCharacter.png').load((loader, resources) => {
+    const PLAYER = new PIXI.Sprite(resources.PLAYER.texture);
+
+    PLAYER.anchor.set(0.5);
+    PLAYER.x = app.renderer.width / 2;
+    PLAYER.y = app.renderer.height / 2;
+
+    app.stage.addChild(PLAYER);
+}); */
