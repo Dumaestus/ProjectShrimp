2c1e90fef267cb1f845a4980f1010d2ba537c4cd
regular space <thomaskaygames@gmail.com>
2024-01-09 20:42:29 -0500

    Added initial work for game loop
    - Started work on game loop
    - Autosize canvas css

--- src/js/game.js
+++ src/js/game.js
@@ -1,25 +1,59 @@
-// Create PIXI Application
-const app = new PIXI.Application({
-    width: 320,
-    height: 180
-});
+// Game code
 
-// Sets scale mode to nearest
-PIXI.BaseTexture.defaultOptions.scaleMode = 0;
-PIXI.BaseTexture.defaultOptions.mipmap = 2;
+const screenWidth = 320;
+const screenHeight = 180;
+const STATE = {
+    TITLE: 0,
+    GAMEPLAY: 1,
+    DEATH: 2
+}
+let currentState = STATE.TITLE;
 
-// Add the PIXI canvas to the HTML body
-document.body.appendChild(app.view)
+// Begin game
 
-const background = PIXI.Sprite.from('images/game_background.png'); // Initializes game background image
-const player = PIXI.Sprite.from('images/player/player_right1.png') // Initializes player character image
+Init()
+GameLoop(currentState)
 
-app.stage.addChild(background)
 
-app.stage.addChild(player)
+function Init() {
+    // Create PIXI Application
+    const app = new PIXI.Application({
+        width: screenWidth,
+        height: screenHeight
+    });
 
-player.anchor.set(0.5) // center the player's anchor point
+    // Sets scale mode to nearest
+    PIXI.BaseTexture.defaultOptions.scaleMode = 0;
+    PIXI.BaseTexture.defaultOptions.mipmap = 2;
 
-// Moves the sprite to the center of the screen
-player.x = app.screen.width / 2 
-player.y = app.screen.height / 2
+    // Add the PIXI canvas to the HTML body
+    document.body.appendChild(app.view);
+
+    const background = PIXI.Sprite.from('images/game_background.png');
+    //background.scale.set(2,2);
+    app.stage.addChild(background);
+
+    const player = PIXI.Sprite.from('images/player/player_right1.png');
+    //player.scale.set(2,2);
+    player.x = app.screen.width / 2;
+    player.y = app.screen.height / 2;
+
+    app.stage.addChild(player);
+
+}
+
+function GameLoop(state) {
+    //console.log(state);
+
+    switch(state) {
+        case STATE.TITLE:
+            break;
+
+        case STATE.GAMEPLAY:
+            break;
+
+        case STATE.DEATH:
+            break;
+            
+    }
+}
