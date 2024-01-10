014e366955065fd2a55916b44d0c094f94a72ad4
regular space <thomaskaygames@gmail.com>
2024-01-09 23:08:21 -0500

    Broke out setup

--- src/js/game.js
+++ src/js/game.js
@@ -37,7 +37,7 @@ function Init() {
 
     // Sets scale mode to nearest
     PIXI.BaseTexture.defaultOptions.scaleMode = 0;
-    PIXI.BaseTexture.defaultOptions.mipmap = 2;
+    //PIXI.BaseTexture.defaultOptions.mipmap = 2;
 
     // Add the PIXI canvas to the HTML body
     document.body.appendChild(app.view);
@@ -109,6 +109,8 @@ function Update() {
 function UpdateTitle() {
     // Write title screen code here
 
+
+    // Should switch to GAMEPLAY
     //Object.onkeydown = ChangeState(STATE.GAMEPLAY);
 
 }
@@ -135,30 +137,21 @@ function Setup(currentState) {
     switch(currentState) {
         
         case STATE.TITLE:
-            titleScreen = PIXI.Sprite.from('images/title_placeholder.png');
-            app.stage.addChild(titleScreen);
+            SetupTitle();
 
             console.log(elapsed + ": Setup(STATE.TITLE) complete!");
             break;
 
         case STATE.GAMEPLAY: {
-            // Initialize background
-            background = PIXI.Sprite.from('images/game_background.png');
-            app.stage.addChild(background);
-
-            // Initialize player
-            // TODO: Animate player
-            player = PIXI.Sprite.from('images/player/player_right1.png');
-            player.x = app.screen.width / 2;
-            player.y = app.screen.height / 2;
-
-            app.stage.addChild(player);
+            SetupGameplay();
 
             console.log(elapsed + ": Setup(STATE.GAMEPLAY) complete!");
             break;
         }
 
         case STATE.DEATH: {
+            SetupDeath();
+
             console.log(elapsed + ": Setup(STATE.DEATH) complete!");
             break;
 
@@ -171,6 +164,31 @@ function Setup(currentState) {
             break;
     }
     return true;
+}
+
+function SetupTitle() {
+    titleScreen = PIXI.Sprite.from('images/title_placeholder.png');
+    app.stage.addChild(titleScreen);
+}
+
+function SetupGameplay() {
+    // Initialize background
+    background = PIXI.Sprite.from('images/game_background.png');
+    app.stage.addChild(background);
+    
+    // Initialize player
+     // TODO: Animate player
+    player = PIXI.Sprite.from('images/player/player_right1.png');
+    player.x = app.screen.width / 2;
+    player.y = app.screen.height / 2;
+    
+    app.stage.addChild(player);
+
+}
+
+function SetupDeath() {
+    
+
 }
 
 // Reset the game
